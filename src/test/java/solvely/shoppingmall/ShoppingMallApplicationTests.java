236d311cc9f960e33a69dff0d76d14c592ce6f36
SJ Jeon <ayclub8866@naver.com>
2022-10-16 15:38:35 +0900

    first commit

---
+++ src/test/java/solvely/shoppingmall/ShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package solvely.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
